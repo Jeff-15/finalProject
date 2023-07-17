9897ddc3ec59fea8d8525d5330a85e22587757da
jerryzhang1011 <131218857+jerryzhang1011@users.noreply.github.com>
2023-07-17 13:24:46 -0400

    test

--- main.cc
+++ main.cc
@@ -1,10 +1,14 @@
 #include <iostream>
 
 
+void print_board() {
+    std::cout << "I love Jeff" << std::endl;
+}
+
 
 int main (int argc, char* argv[]) {
 
     std::cout << "Hello World" << std::endl;
-    
+    print_board();
     return 0;
 }
