16ee30cf10be870695baf0fc52a4fc5d7686154c
Jeff-15 <jefferyxu200415@gmail.com>
2023-07-20 16:31:35 -0400

    gameboard

---
+++ gameBoard.h
@@ -0,0 +1,22 @@
+#ifndef GAMEBOARD
+#define GAMEBOARD
+#include <vector>
+#include "subject.h"
+class GameBoard: public Subject{
+    void initializeWood();
+    void initializeBrick();
+    void initializeSheep();
+    void initializeDesert();
+    void initialzeStone();
+    void initialzeRolls();
+    std::vector <Dice*> d;
+    std::vector <Tile*> d;
+    std::vector <Vertex*> d;
+    std::vector <Edge*> d;
+
+    public: 
+        void processCommand(int event,int target);
+        void initialize();
+};
+
+#endif
