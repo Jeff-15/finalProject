5c9f04f60836aefb0aae90d57bdbdf88e4f0d153
jerryzhang1011 <131218857+jerryzhang1011@users.noreply.github.com>
2023-07-21 13:06:45 -0400

    1

--- gameBoard.h
+++ gameBoard.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include "dice.h"
 #include "subject.h"
+#include "board.h"
+
 class GameBoard: public Subject{
     virtual void initializeWood() = 0;
     virtual void initializeBrick() = 0;
@@ -11,9 +13,9 @@ class GameBoard: public Subject{
     virtual void initialzeStone() = 0;
     virtual void initialzeRolls() = 0;
     std::vector <Dice*> d;
-    std::vector <Tile*> d;
-    std::vector <Vertex*> d;
-    std::vector <Edge*> d;
+    std::vector <Tile*> T;
+    std::vector <Vertex*> V;
+    std::vector <Edge*> E;
 
     public: 
         void processCommand(int target,int eventPara1, int eventPara2);
